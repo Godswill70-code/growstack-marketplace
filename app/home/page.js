72d5d1f6b35f8b250238add0aae03d087ae8a4d5
Godswill70-code <godswilldigitalz@gmail.com>
2025-07-06 16:55:00 +0100

    Create page.js

---
+++ app/home/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to Growstack Marketplace</h1>
+      <p>This is the beginning of your e-commerce journey.</p>
+    </div>
+  );
+  }
