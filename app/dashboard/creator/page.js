9616c2858912f5d3a9a12858e3d8c3f77e44bfae
Godswill70-code <godswilldigitalz@gmail.com>
2025-07-07 18:35:55 +0100

    Create page.js

---
+++ app/dashboard/creator/page.js
@@ -0,0 +1,8 @@
+export default function CreatorDashboard() {
+  return (
+    <div>
+      <h2>Welcome, Creator 📚</h2>
+      <p>Upload your digital products and manage your sales.</p>
+    </div>
+  );
+}
