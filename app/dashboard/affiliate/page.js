ae02322c88c75eb50b54090866f8812cf29aa693
Godswill70-code <godswilldigitalz@gmail.com>
2025-07-13 21:43:22 +0100

    Update page.js

--- app/dashboard/affiliate/page.js
+++ app/dashboard/affiliate/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-import supabase from '../../utils/supabaseClient';
+import supabase from '../../../utils/supabaseClient';
 
 export default function AffiliateDashboard() {
   const [products, setProducts] = useState([]);
