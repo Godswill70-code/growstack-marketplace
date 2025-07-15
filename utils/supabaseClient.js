fcb34bed3734901b8d1f8d9ae90209054bd3a569
Godswill70-code <godswilldigitalz@gmail.com>
2025-07-15 15:59:00 +0100

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -4,3 +4,8 @@ const supabaseUrl = 'https://qsvfjpbsmvjcrzcyusca.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzdmZqcGJzbXZqY3J6Y3l1c2NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MjA4NjIsImV4cCI6MjA2NzM5Njg2Mn0.bLpVACQ0xY3dMCVZTEGYjui7bOQtAR51-VpwsbQX5QM'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+// ✅ Export both default and named, so both ways work
+export default supabase;
+export { supabase };
