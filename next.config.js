1df09bd194b94963ff76330f52fc335f15d79a32
Godswill70-code <godswilldigitalz@gmail.com>
2025-07-28 20:50:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
- @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const { createNextIntlPlugin } = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.js'); 
